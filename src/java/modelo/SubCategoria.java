ae568d48f7dae5a8c1d1a42b19292bf9d81cdad1
celin <celin@Mayumi>
2017-03-25 14:58:28 -0300

    Adicionado classe subcategoria

---
+++ src/java/modelo/SubCategoria.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class SubCategoria extends Categoria {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
