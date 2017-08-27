e918e21b264f9940fdf8efe13b85a318000c8ef5
ailto <ailto@DESKTOP-5P7717D>
2017-08-27 19:52:11 -0300

    Atualização de da API java.mail.jar e modificado os arquivos que utilizam essa API

--- src/java/emailSender/ThreadEmailSenderCadastro.java
+++ src/java/emailSender/ThreadEmailSenderCadastro.java
@@ -2,7 +2,6 @@ package emailSender;
 
 import modelo.Usuario;
 import java.util.Properties;
-import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -12,13 +11,13 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class ThreadEmailSenderCadastro implements Runnable {
-    
+
     private int id;
     private String nome;
     private String email;
     private String cpf;
     private String senha;
-    
+
     public ThreadEmailSenderCadastro(Usuario usuario) {
         this.nome = usuario.getNome();
         this.email = usuario.getEmail();
@@ -31,40 +30,37 @@ public class ThreadEmailSenderCadastro implements Runnable {
 
     @Override
     public void run() {
-        
+
+        final String username = "projetovendemais@gmail.com";
+        final String password = "60708090";
+
         Properties props = new Properties();
-
-        //Parâmetros de conexão com servidor Gmail
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(props,
+        Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("projetovendemais@gmail.com", "60708090");
+                return new PasswordAuthentication(username, password);
             }
         });
 
-        //Ativa Debug para sessão
-        session.setDebug(true);
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("projetovendemais@gmail.com")); //Remetente
+            message.setFrom(new InternetAddress("from-email@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email));
+            message.setSubject("VendeMais Cadastro Realizado");
+            message.setText("Bem vindo ao VendeMais " + nome + ", sua conta foi criada com sucesso com o login " + email + " e a senha: " + senha + ". Tenha ótimas compras e vendas. Atenciosamente. Equipe VendeMais");
 
-            Address[] toUser = InternetAddress //Destinatário(s)
-                    .parse(email); //Pega o email para enviar
-            message.setRecipients(Message.RecipientType.TO, toUser);
-            message.setSubject("VendeMais Cadastro Realizado");//Assunto
-            message.setText("Bem vindo ao VendeMais" + nome + ", sua conta foi criada com sucesso com o login " + email + " e a senha: " + senha + ". Tenha ótimas compras e vendas. Atenciosamente. Equipe VendeMais");
-
-            //Método para enviar a mensagem criada
             Transport.send(message);
+
         } catch (MessagingException e) {
             throw new RuntimeException(e);
-        }      
-    }    
+        }
+
+    }
 }
--- src/java/emailSender/ThreadEmailSenderComprador.java
+++ src/java/emailSender/ThreadEmailSenderComprador.java
@@ -30,27 +30,27 @@ public class ThreadEmailSenderComprador implements Runnable {
     private int anuncioQuantidade;
     private double anuncioPreco;
     private String anuncioFormaEnvio;
-    
+
     private int compradorId;
     private String compradorNome;
     private String compradorEmail;
-    
+
     private int vendedorId;
     private String vendedorNome;
     private String vendedorEmail;
     private int qtd;
-    
+
     public ThreadEmailSenderComprador(Comprador comprador, Vendedor vendedor, Anuncio anuncio, Compra compra) {
         this.anuncioTitulo = anuncio.getTitulo();
         this.anuncioDescricao = anuncio.getDescricao();
         this.anuncioQuantidade = anuncio.getQuantidade();
         this.anuncioPreco = anuncio.getPreco();
         this.anuncioFormaEnvio = anuncio.getDescricao();
-        
+
         this.compradorId = comprador.getId();
         this.compradorNome = comprador.getNome();
         this.compradorEmail = comprador.getEmail();
-        
+
         this.vendedorId = vendedor.getId();
         this.vendedorNome = vendedor.getNome();
         this.vendedorEmail = vendedor.getEmail();
@@ -61,42 +61,38 @@ public class ThreadEmailSenderComprador implements Runnable {
 
     @Override
     public void run() {
-        
+
+        final String username = "projetovendemais@gmail.com";
+        final String password = "60708090";
+
         Properties props = new Properties();
-
-        //Parâmetros de conexão com servidor Gmail
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(props,
+        Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("projetovendemais@gmail.com", "60708090");
+                return new PasswordAuthentication(username, password);
             }
         });
 
-        //Ativa Debug para sessão
-        session.setDebug(true);
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("projetovendemais@gmail.com")); //Remetente
-
-            Address[] toUser = InternetAddress //Destinatário(s)
-                    .parse(compradorEmail); //Pega o email para enviar
-            message.setRecipients(Message.RecipientType.TO, toUser);
-            message.setSubject("VendeMais Compra Realizada");//Assunto
+            message.setFrom(new InternetAddress("from-email@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(compradorEmail));
+            message.setSubject("VendeMais Compra Realizada");
             message.setText("Olá " + compradorNome + ", o pedido de compra do produto " + anuncioTitulo + " do vendedor " + vendedorNome + " foi realizado.");
 
-            //Método para enviar a mensagem criada
             Transport.send(message);
+
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
-        
+
     }
-    
+
 }
